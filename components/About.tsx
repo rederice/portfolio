9a5c985ac9d6c58aedc455c9bfd59a822aa1dd6d
rederice <digcent@yahoo.com.tw>
2022-12-06 05:45:04 +0800

    add about section

---
+++ components/About.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+type Props = {}
+
+export default function About({}: Props) {
+  return (
+    <motion.div 
+    initial={{ opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1.5}}
+    className='relative flex flex-col justify-evenly items-center mx-auto px-10 
+    md:flex-row max-w-full overflow-hidden h-screen'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-[#71d7ff] font-[1100] text-2xl'>
+            About
+        </h3>
+
+        <div className='flex pt-24 space-x-5 w-[500px] md:w-[700px] xl:w-[800px] items-center
+        snap-x snap-mandatory h-[60%]'>
+          <motion.img
+            initial={{opacity: 0,}}
+            transition={{duration: 1.2}}
+            whileInView={{opacity: 1}}
+            viewport={{once: true}}
+            className="absolute w-[500px] md:w-[700px] xl:w-[800px] justify-center mx-auto"
+            src="portfolio/coord.png"
+            ></motion.img>
+        </div>
+    </motion.div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import Head from 'next/head'
 import Header from "../components/Header"
 import Hero from '../components/Hero'
 import Projects from '../components/Projects'
+import About from '../components/About'
 
 export default function Home() {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen 
     snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon.ico`}></meta>
         <link rel="icon" href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon.ico`}></link>
         <title>Hung-Fang&apos;s Portfolio</title>
       </Head>
@@ -27,6 +29,9 @@ export default function Home() {
       </section>
       
       {/* Resume */}
+      <section id="resume" className='snap-center'>
+        <About />
+      </section>
     </div>
   )
 }
