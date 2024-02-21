8d11993191c3607ee4ee7dc66a11122b74841cff
rederice <digcent@yahoo.com.tw>
2024-02-21 12:55:59 +0000

    Deploying to gh-pages from @ rederice/portfolio@3f28c036ca0812bc62aedd667f855034e8a4c8f8 🚀

--- _next/static/lguUvc_1N6REyBcsICt7v/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/788-d249e24cf40d43f9.js","static/chunks/pages/index-7d51193905bb277c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
