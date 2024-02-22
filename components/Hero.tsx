39bc53c9e5f1772abcd22beb8e73454c9f725c54
Vincent Chen <doroby@VincentdeAir.lan>
2024-02-22 16:34:48 -0700

    update hero visual

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,26 +9,26 @@ const imageLoader = require("../pages/api/imageLoader.js");
 
 export default function Hero({}: Props) {
   return (
-    <div className='flex flex-col space-y-16 justify-start px-[8vw] h-screen'>
+    <div className='flex flex-col space-y-20 justify-start px-[8vw] h-screen'>
         <div className="flex flex-row text-black font-bold pt-[10vh] leading-tight
          xl:text-8xl md:text-8xl text-6xl">
-            <p>User Experience<br/>
-            Researcher<span className="text-lavendar">.</span></p>
+            <p>UI/UX Designer<br/>
+            & Researcher<span className="text-lavendar">.</span></p>
         </div>
 
         <div className="flex flex-col items-end leading-tight text-black
          xl:text-4xl md:text-3xl text-xl">
-            <div className="flex flex-row">Hello! I am&nbsp;<div className="font-bold">Hung-Fang</div>, a passionate UX researcher</div>
-            <div className="flex flex-row items-end">with &nbsp;
-                <div className="font-semibold text-shadow-lg shadow-lavendar
+            <div className="flex flex-row">Hello! I am&nbsp;<div className="font-bold">Hung-Fang</div>, a passionate web developer</div>
+            <div className="flex flex-row items-end">with&nbsp;
+                <div className="font-semibold text-lavendar
                 xl:text-6xl md:text-5xl text-2xl">Software Product Design</div>
             </div>
             <div className="flex flex-row">experience.</div>
         </div>
 
         <div className="flex flex-row justify-center pt-4">
-            <button className="px-6 py-4 rounded-md bg-lavendar-semilight text-shadow-sm shadow-black shadow-md
-            hover:bg-lavendar xl:text-4xl md:text-4xl text-2xl"><a href="/portfolio/Resume_HungfangChen.pdf" target="_blank">View Résumé</a></button>
+            <button className="px-6 py-4 rounded-full bg-gradient-to-r from-lavendar-semilight to-blue-500
+            hover:ring-lavendar hover:ring-2 hover:animate-pulse duration-200 xl:text-3xl md:text-3xl text-2xl"><a href="/portfolio/Resume_HungfangChen.pdf" target="_blank">View Résumé</a></button>
         </div>
     </div>
   )
