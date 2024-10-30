2213f4e0f14a24738e6c1f54f67460cdbf835cd6
rederice <digcent@yahoo.com.tw>
2024-10-30 11:39:36 +0000

    Deploying to gh-pages from @ rederice/portfolio@75f1f3c2ea95af1336fcaf7dbfe956dc5710f83e 🚀

---
+++ _next/static/0y1n2zy9o63BDFmQJCbgy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/788-d249e24cf40d43f9.js","static/chunks/pages/index-20458090c7289a67.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/q4CsmG8DfblJldemH0NNc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/788-d249e24cf40d43f9.js","static/chunks/pages/index-541fd698ac7248dc.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
